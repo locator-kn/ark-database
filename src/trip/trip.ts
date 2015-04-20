bcc66c31dcec1f867a552e9f22b6f92073f54dec
T1m1 <Ruprecht.t@gmx.de>
2015-04-20 13:38:35 +0200

    implemnet function to get a trip by specific trip id.

--- src/trip/trip.ts
+++ src/trip/trip.ts
@@ -12,4 +12,14 @@ class Trip {
     getTrips = (callback) => {
         this.db.view(this.VIEWS.VIEW_TRIP_TRIP, callback);
     };
+
+    /**
+     * Get trip from database by specific trip id.
+     *
+     * @param tripid:string
+     * @param callback
+     */
+    getTripById = (tripid:string, callback) => {
+        this.db.view(this.VIEWS.VIEW_TRIP_TRIP, {key: tripid}, callback);
+    };
 }
