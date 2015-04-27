6fd1c9785ab0c22ad35d7377e313b3ad99b647bd
T1m1 <Ruprecht.t@gmx.de>
2015-04-27 13:18:47 +0200

    fix naming

--- src/plugin.ts
+++ src/plugin.ts
@@ -44,7 +44,7 @@ class Database {
     constructor(database:string, url?:string, port?:number) {
         // register plugin
         this.register.attributes = {
-            name: 'backend-database',
+            name: 'ark-database',
             version: '0.1.0'
         };
 
