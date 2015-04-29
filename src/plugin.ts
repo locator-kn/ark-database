668f11a73bf2b2b45d7e96df4f41679d756d4fe2
T1m1 <Ruprecht.t@gmx.de>
2015-04-29 10:54:38 +0200

    remove unused field views from constructor

--- src/plugin.ts
+++ src/plugin.ts
@@ -88,7 +88,7 @@ class Database {
 
         this.user = new User(this.db, this.VIEWS, this.LISTS);
         this.trip = new Trip(this.db, this.VIEWS, this.LISTS);
-        this.location = new Location(this.db, this.VIEWS, this.LISTS);
+        this.location = new Location(this.db, this.LISTS);
         this.staticdata = new StaticData(this.db, this.LISTS);
     };
 
--- src/trip/trip.ts
+++ src/trip/trip.ts
@@ -1,6 +1,6 @@
 export default
 class Trip {
-    constructor(private db:any, private VIEWS:any, private LISTS:any) {
+    constructor(private db:any, private LISTS:any) {
     }
 
     /**
