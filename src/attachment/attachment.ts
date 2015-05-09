435e3fcd659d78676b73bae15e32ea6a06bfe739
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-05-09 14:13:42 +0200

    implement method for saving picture

--- src/attachment/attachment.ts
+++ src/attachment/attachment.ts
@@ -2,6 +2,7 @@ export default
 class Attachment {
     Readable:any;
     stream:any;
+
     constructor(private db:any, private VIEWS:any, private LISTS:any) {
         this.stream = require('stream');
         this.Readable = this.stream.Readable ||
@@ -10,19 +11,50 @@ class Attachment {
 
     /**
      * Returns the picture (attachment) with that name to that corresponding document
+     * @param documentid
      * @param userid
      * @param callback
      */
     getPicture = (documentid:string, filename:string, callback) => {
 
-        var stream = this.db.getAttachment(documentid,filename, err => {
+        return new this.Readable().wrap(this.db.getAttachment(documentid, filename, (err) => {
+            // TODO: log error
+        }));
+
+    };
+
+    /**
+     *
+     * @param documentId
+     * @param filename
+     * @param readStream
+     * @param callback
+     */
+    savePicture = (documentId:string, filename:string, readStream:any, callback) => {
+
+        var attachmentData = {
+            name: filename,
+            'Content-Type': 'multipart/form-data'
+        };
+
+
+        // get revision from database
+        this.db.head(documentId, (err, data) => {
+
             if (err) {
-                callback(err);
-                return;
+                return callback(err);
             }
+
+            var idData = {
+                _id: documentId,
+                _rev: data.etag.split('"')[1]
+            };
+
+            var writeStream = this.db.saveAttachment(idData, attachmentData, callback);
+
+            readStream.pipe(writeStream);
         });
 
-        // wrap stream and return it
-        callback(null, new this.Readable().wrap(stream));
-    };
+
+    }
 }
