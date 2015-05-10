eab32d8a4bb8eb7be380b2346d3bac233860d3cd
T1m1 <Ruprecht.t@gmx.de>
2015-05-10 22:53:25 +0200

    replace create view method in new file

--- src/util/util.ts
+++ src/util/util.ts
@@ -12,4 +12,15 @@ class Util {
     updateDocument = (documentid:string, object:any, callback) => {
         this.db.merge(documentid, object, callback)
     };
+
+    /**
+     * Create a view or list
+     * @param name
+     * @param views
+     * @param callback
+     */
+    createView = (name:string, views, callback) => {
+        this.db.save(name, views);
+        callback(null, 'View created!');
+    }
 }
