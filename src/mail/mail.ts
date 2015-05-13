30278ed82d34b7efaa8f070dc5dba9d0f3fc1d95
T1m1 <Ruprecht.t@gmx.de>
2015-05-13 12:26:23 +0200

    implement function to get registration mail infomrations

--- src/mail/mail.ts
+++ src/mail/mail.ts
@@ -3,5 +3,7 @@ class Mail {
     constructor(private db:any, private LISTS:any) {
     }
 
-
+    getRegistrationMail = (callback) => {
+        this.db.list(this.LISTS.LIST_MAIL_REGISTRATION,  callback);
+    }
 }
