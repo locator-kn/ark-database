52e459eb84604aefde3e3999c3e4cfac1648191e
T1m1 <Ruprecht.t@gmx.de>
2015-05-13 12:42:52 +0200

    add doc

--- src/mail/mail.ts
+++ src/mail/mail.ts
@@ -3,6 +3,10 @@ class Mail {
     constructor(private db:any, private LISTS:any) {
     }
 
+    /**
+     * get text and subject etc. from database for registration mail
+     * @param callback
+     */
     getRegistrationMail = (callback) => {
         this.db.list(this.LISTS.LIST_MAIL_REGISTRATION,  callback);
     }
