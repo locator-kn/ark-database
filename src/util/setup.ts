efc1544f5e7fb48a98776a3c5602402364b477d6
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-05-18 15:27:40 +0200

    implement set up functionallity for creating all needed views

---
+++ src/util/setup.ts
@@ -0,0 +1,117 @@
+export function setUpDesignDocuments(database:any, callback:any) {
+
+    database.save(designUser.title, designUser.content);
+
+    database.save(designData.title, designData.content);
+
+    database.save(location.title, location.content);
+
+    database.save(static.title, static.content);
+
+    database.save(trip.title, trip.content)
+
+    callback(null, "Views created")
+
+
+}
+
+// user design document
+var designUser = {
+    title: "_design/user",
+    content: {
+        "language": "javascript",
+        "views": {
+            "login": {
+                "map": "function(doc) {\n if(doc.type == 'user') {\n   emit(doc._id, {\n    password: doc.password\n   });\n }\n}"
+            },
+            "user": {
+                "map": "function(doc) {\n if(doc.type == 'user') {\n   emit(doc._id, {\n    _rev: doc._rev,\n    picture: doc.picture,\n    name: doc.name, \n    surname: doc.surname,\n    mail: doc.mail,\n    });\n }\n}"
+            }
+        },
+        "lists": {
+            "listall": "function (head, req) { var row; var result = []; while (row = getRow()) { result.push(row.value); } send(JSON.stringify(result)); }"
+        }
+    }
+};
+
+var designData = {
+    title: "_design/data",
+    content: {
+        "language": "javascript",
+        "views": {
+            "moods": {
+                "map": "function(doc) {\n if(doc.type== 'mood') {\n   emit(doc._id, doc);\n   }\n}"
+            },
+            "cities": {
+                "map": "function(doc) {\n if(doc.type== 'city') {\n   emit(doc._id, doc);\n   }\n}"
+            },
+            "accommodations": {
+                "map": "function(doc) {\n if(doc.type== 'accommodation') {\n   emit(doc._id, doc);\n   }\n}"
+            }
+        },
+        "lists": {
+            "listall": "function (head, req) { var row; var result = []; while (row = getRow()) { result.push(row.value); } send(JSON.stringify(result)); }"
+        }
+    }
+};
+
+var location = {
+    title: "_design/location",
+    content: {
+        "language": "javascript",
+        "views": {
+            "location": {
+                "map": "function(doc) {\n if (doc.type == 'location') {\n  emit(doc._id, doc);\n }\n}"
+            },
+            "user": {
+                "map": "function(doc) {\n if (doc.type == 'location') {\n  emit(doc.userid, doc);\n }\n}"
+            }
+        },
+        "lists": {
+            "listall": "function (head, req) { var row; var result = []; while (row = getRow()) { result.push(row.value); } send(JSON.stringify(result)); }"
+        }
+    }
+};
+
+var static = {
+    title: "_design/static",
+    content: {
+        "language": "javascript",
+        "views": {
+            "moods": {
+                "map": "function(doc) {\n if(doc.type== 'mood') {\n   emit(doc._id, doc);\n   }\n}"
+            }
+        },
+        "lists": {
+            "listall": "function (head, req) { var row; var result = []; while (row = getRow()) { result.push(row.value); } send(JSON.stringify(result)); }"
+        }
+    }
+};
+
+var trip = {
+    title: "_design/trip",
+    content: {
+        "language": "javascript",
+        "views": {
+            "trip": {
+                "map": "function(doc) {\n if(doc.type == 'trip') {\n   emit(doc._id, doc);\n   }\n}"
+            },
+            "city": {
+                "map": "function(doc) {\n if(doc.type == 'trip') {\n   emit(doc.city, doc);\n   }\n}"
+            },
+            "citybyid": {
+                "map": "function(doc) {\n if(doc.type == 'trip') {\n   emit(doc.city, doc._id);\n   }\n}",
+                "reduce": "function(keys, values) {\n                         var voucherTypes = [];\n                         values.forEach(function(v) {\n                             voucherTypes = voucherTypes.concat(v);\n                         });\n                         return voucherTypes;\n                     }"
+            },
+            "search": {
+                "map": "function(doc) {\n if(doc.type == 'trip') {\n   emit([doc.city, doc.category], doc);\n   }\n}"
+            },
+            "search01": {
+                "map": "function(doc) {\n if(doc.type == 'trip') {\nvar a = 0;\n\tdoc.category.forEach(function(entry) {\nemit([doc.city, doc.category[a]], doc);\na++;\n});\n   \n   }\n}"
+            }
+        },
+        "lists": {
+            "listall": "function (head, req) { var row; var result = []; while (row = getRow()) { result.push(row.value); } send(JSON.stringify(result)); }"
+        }
+    }
+};
