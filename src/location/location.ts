220a1d8b532c6537ffd646771998b7395849002d
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-08 17:08:37 +0200

    use correct db list

--- src/location/location.ts
+++ src/location/location.ts
@@ -33,7 +33,7 @@ class Location {
     getPreLocationsByUserId = (userid:string, callback) => {
         return new Promise((reject, resolve) => {
 
-            this.db.list(this.LISTS.LIST_LOCATION_USER, {key: userid}, (err, data) => {
+            this.db.list(this.LISTS.LIST_LOCATION_PRELOCATION_USER, {key: userid}, (err, data) => {
 
                 if (err) {
                     return reject(err);
