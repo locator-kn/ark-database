a1a33339b082d621eb60333e3dabec5dbac2d0b0
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-06-20 13:54:58 +0200

    create logging methods

---
+++ src/logging/logging.ts
@@ -0,0 +1,14 @@
+var server;
+
+export function initLogging(hapiServer:any) {
+    server = hapiServer;
+}
+
+
+export function log(logging:string) {
+    if (!server) {
+        console.error('Server not initialized for logging');
+        return;
+    }
+    server.log(logging);
+}
