d654bafd7937260d5f7e492388e7a3758e3f2dd8
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-07-02 09:58:50 +0200

    add creating default data to the setup process

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,8 @@
-import {setUpDesignDocuments} from './util/setup'
+import {setUpDesignDocuments, createDefaultLocationAndUser} from './util/setup'
 
 export function setup(database:any, callback:any) {
 
     setUpDesignDocuments(database, callback);
+
+    createDefaultLocationAndUser(database, callback);
 }
