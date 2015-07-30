ac56e716aa2af4ec16121d3b9f9eac4bc30ac12f
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-07-30 10:09:49 +0100

    implement related locations

--- src/location/location.ts
+++ src/location/location.ts
@@ -216,10 +216,28 @@ class Location {
      * @param number
      */
     getRelatedLocations = (locationid:string, number:number) => {
-        return new Promise((resolve, reject) => {
 
-            
-        })
+        // first get the city of location
+        return this.util.getDocument(locationid)
+            .then(value => {
+                // get all locations from city TODO: find better solution, not scalable
+                return this.getLocationsByCity(value.city.place_id);
+            }).then(locations => {
+                // get "number" random elements and return them
+
+                var length = locations.length;
+                var resultArray = [];
+
+                for (var i = 0; i < number; i++) {
+                    var randomIndex = Math.floor(Math.random() * (length - i - 1));
+                    resultArray.push(locations[randomIndex]);
+
+                    // remove entry to avoid duplicate entries in result array
+                    locations.splice(randomIndex, 1);
+                }
+
+                return Promise.resolve(resultArray)
+            })
     };
 
     /**
